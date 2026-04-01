12a8038396c252a51f33ae8a30289962de08c9f5
meishibiezb <750783119@qq.com>
2026-04-01 20:03:18 +0800

    feat: 加入pyautogui和tkinter包

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import pyautogui
+import tkinter as tk
+
+def create_overlay():
+    root = tk.Tk()
+    root.title = "你妈妈死掉了"
+    root.geometry("400x400+100+100")
+    root.attributes('-topmost', True)
+    root.config(bg='gray20')
+    root.attributes('-transparentcolor', 'gray20')
+    # root.overrideredirect(True)
+
+    root.mainloop()
+
 def main():
     print("Hello from my-rpa!")
-
+    screenshot = pyautogui.screenshot();
+    screenshot.save("screenshot.png");
+    create_overlay()
 
 if __name__ == "__main__":
     main()
