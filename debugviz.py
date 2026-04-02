d0961fbcf1ff407160d748d951b1b0bd63a920b6
meishibiezb <750783119@qq.com>
2026-04-02 21:19:21 +0800

    feat: 创建debugviz类

---
+++ debugviz.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+import keyboard
+import threading
+
+class DebugViz:
+    def __init__(self):
+
+        self.root = None
+        self.canvas = None
+        self.is_running = False
+
+    def start(self):
+        self.is_running = True
+
+        # 设置在另一个线程中运行GUI
+        self.thread = threading.Thread(target=self._run_gui, daemon=True)
+        self.thread.start
+
+    def _run_gui(self):
+        # 设置窗口基本属性
+        self.root = tk.Tk()
+        self.root.title = "title"
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        self.root.geometry(f"{screen_width}x{screen_height}+0+0")
+        self.root.attributes('-topmost', True)
+        self.root.config(bg='gray20')
+        self.root.attributes('-transparentcolor', 'gray20')
+        self.root.overrideredirect(True)
+
+        # 设置画布
+        self.canvas = tk.Canvas(self.root, bg='gray20', highlightthickness=0)
+        self.canvas.pack(expand=True, fill='both')
+        w = 400
+        h = 400
+        border_width = 2
+        self.canvas.create_rectangle(
+            border_width, border_width, 
+            w - border_width, h - border_width, 
+            outline='lime',
+            width=2
+        )
+        self.canvas.create_text(w/2, h/2, text="0", fill='white', font=('Arial', 16))
+
+        # 设置退出快捷键（以及定义回调函数）
+        def close_window(root):
+            print("关闭窗口")
+            root.destroy()
+        keyboard.on_press_key("esc", lambda : close_window(self.root))
+
+        self.root.mainloop()
+
+    def update_title(self, text):
+        if self.root:
+            def title(root, text):
+                root.title = text
+            self.root.after(0, lambda: title(text=text))
